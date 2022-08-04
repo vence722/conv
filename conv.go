6573c49933562bd8f0067b04cd2c6b6415e9f47d
Vence Lin <vence.lin@regtics.com>
2022-08-04 11:16:07 +0800

    add uint and float conversion functions

--- conv.go
+++ conv.go
@@ -27,3 +27,42 @@ func Str2IntOrElse[T SignedInterger](input string, defaultValue T) T {
 		return res
 	}
 }
+
+func Str2Uint[T UnsignedInteger](input string) (T, error) {
+	if res, err := strconv.ParseUint(input, 10, 64); err != nil {
+		return 0, err
+	} else {
+		return T(res), nil
+	}
+}
+
+func Str2UintOrElse[T UnsignedInteger](input string, defaultValue T) T {
+	if res, err := Str2Uint[T](input); err != nil {
+		return defaultValue
+	} else {
+		return res
+	}
+}
+
+func Float2Str[T Float](input T, prec ...int) string {
+	if len(prec) > 0 {
+		return strconv.FormatFloat(float64(input), 'f', prec[0], 64)
+	}
+	return strconv.FormatFloat(float64(input), 'g', 6, 64)
+}
+
+func Str2Float[T Float](input string) (T, error) {
+	if res, err := strconv.ParseFloat(input, 64); err != nil {
+		return 0, err
+	} else {
+		return T(res), nil
+	}
+}
+
+func Str2FloatOrElse[T Float](input string, defaultValue T) T {
+	if res, err := Str2Float[T](input); err != nil {
+		return defaultValue
+	} else {
+		return res
+	}
+}
--- conv_test.go
+++ conv_test.go
@@ -1,6 +1,9 @@
 package conv
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestInt2Str(t *testing.T) {
 	var a1 int = -10
@@ -69,7 +72,7 @@ func TestStr2Int(t *testing.T) {
 
 	var a3 string = "a-10"
 	if _, err := Str2Int[int32](a3); err == nil {
-		t.Errorf("expect a3 convertion failed")
+		t.Errorf("expect a3 conversion failed")
 	}
 }
 
@@ -86,6 +89,83 @@ func TestStr2IntOrElse(t *testing.T) {
 
 	var a3 string = "a-10"
 	if res := Str2IntOrElse(a3, int16(20)); res != int16(20) {
-		t.Errorf("expect a3 is converted into 20 when convertion failed")
+		t.Errorf("expect a3 is converted into 20 when conversion failed")
+	}
+}
+
+func TestStr2Uint(t *testing.T) {
+	var a1 string = "10"
+	if res, err := Str2Uint[uint](a1); err != nil || res != 10 {
+		t.Errorf("expect a1 is converted into 10")
+	}
+
+	var a2 string = "10"
+	if res, err := Str2Uint[uint64](a2); err != nil || res != uint64(10) {
+		t.Errorf("expect a2 is converted into 10")
+	}
+
+	var a3 string = "a-10"
+	if _, err := Str2Uint[uint32](a3); err == nil {
+		t.Errorf("expect a3 conversion failed")
+	}
+}
+
+func TestStr2UintOrElse(t *testing.T) {
+	var a1 string = "10"
+	if res := Str2UintOrElse(a1, uint(20)); res != 10 {
+		t.Errorf("expect a1 is converted into 10")
+	}
+
+	var a2 string = "10"
+	if res := Str2UintOrElse(a2, uint64(20)); res != uint64(10) {
+		t.Errorf("expect a2 is converted into 10")
+	}
+
+	var a3 string = "a-10"
+	if res := Str2UintOrElse(a3, uint16(20)); res != uint16(20) {
+		t.Errorf("expect a3 is converted into 20 when conversion failed")
+	}
+}
+
+func TestFloat2Str(t *testing.T) {
+	var a1 float32 = 0.645
+	if res := Float2Str(a1); res != "0.645" {
+		t.Errorf("expect a1 is converted into \"0.645\"")
+	}
+
+	var a2 float32 = -0.654353534534
+	if res := Float2Str(a2, 4); res != "-0.6544" {
+		fmt.Println(res)
+		t.Errorf("expect a2 is converted into \"-0.6543\"")
+	}
+
+	var a3 float64 = -34534.9823567982375
+	if res := Float2Str(a3, 4); res != "-34534.9824" {
+		fmt.Println(res)
+		t.Errorf("expect a3 is converted into \"-34534.9824\"")
+	}
+}
+
+func TestStr2Float(t *testing.T) {
+	var a1 string = "10.567"
+	if res, err := Str2Float[float32](a1); err != nil || res != 10.567 {
+		t.Errorf("expect a1 is converted into 10.567")
+	}
+
+	var a2 string = "-4325.1241414"
+	if res, err := Str2Float[float64](a2); err != nil || res != float64(-4325.1241414) {
+		t.Errorf("expect a2 is converted into -4325.1241414")
+	}
+}
+
+func TestStr2FloatOrElse(t *testing.T) {
+	var a1 string = "-10.567"
+	if res := Str2FloatOrElse(a1, 3.0); res != -10.567 {
+		t.Errorf("expect a1 is converted into -10.567")
+	}
+
+	var a2 string = "-aa325.1241414"
+	if res := Str2FloatOrElse(a2, float64(3.0)); res != float64(3.0) {
+		t.Errorf("expect a2 is converted into 3.0 when conversion failed")
 	}
 }
